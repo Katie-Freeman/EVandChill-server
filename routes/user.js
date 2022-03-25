b740d18305c3f902dd4290ad26acaf6ad8563926
Katie Freeman <katiefreeman@carltons-mbp.lan>
2022-03-25 11:15:10 -0400

    changes to user.js

--- routes/user.js
+++ routes/user.js
@@ -56,8 +56,8 @@ router.post("/register", async (req, res) => {
   }
 });
 
-router.delete('/favorites', (req, res) => {
+// router.delete('/favorites', (req, res) => {
     
-})
+// })
 
 module.exports = router;
