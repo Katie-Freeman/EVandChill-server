4c22198350b82085059b452c6c5fb74fd449fe6b
Katie Freeman <katiefreeman@carltons-mbp.lan>
2022-03-31 14:30:23 -0400

    testing

--- util/locationToCoords.js
+++ util/locationToCoords.js
@@ -1,20 +1,26 @@
 
 require("dotenv").config();
-const axios = require('axios');
+const axios = require("axios");
 
 const latLongFromLocation = async (location) => {
-    try {
-        console.log('fetching data...')
-        const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?key=${process.env.GOOGLE_MAPS_API_KEY}&address=${location}&region=us`);
-        const lat = parseFloat(response.data.results[0].geometry.location.lat.toFixed(1));
-        const lng = parseFloat(response.data.results[0].geometry.location.lng.toFixed(1));
-        console.log(lat, lng);
-        return { lat: lat, lng: lng };
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    console.log("fetching data...");
+    const response = await axios.get(
+     `https://maps.googleapis.com/maps/api/geocode/json?key=${process.env.GOOGLE_MAPS_API_KEY}&address=${location}&region=us`
+    );
+    
+    const lat = parseFloat(
+      response.data.results[0].geometry.location.lat.toFixed(1)
+    );
+    const lng = parseFloat(
+      response.data.results[0].geometry.location.lng.toFixed(1)
+    );
+    console.log(lat, lng);
+    return { lat: lat, lng: lng };
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 module.exports = latLongFromLocation;
-
 
