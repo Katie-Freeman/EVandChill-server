855d4016a6861757c80427158b95a49ae72ae05b
jon-cundiff <jon.cundiff@gmail.com>
2022-04-02 20:47:51 -0500

    adds favorites to guestReset

--- util/guestReset.js
+++ util/guestReset.js
@@ -11,7 +11,36 @@ const task = new Task(
             { username: "GUEST" },
             {
                 email: "guest@test.com",
-                favorites: [],
+                favorites: [
+                    {
+                        stationId: 158529,
+                        title: "PIVET",
+                        address: "2244 Metropolitan Pkwy SW Atlanta, GA 30315",
+                        user: "623de9958c010352ed1e3958",
+                        _id: "6248fb38e3c134faa1968df0",
+                    },
+                    {
+                        stationId: 121844,
+                        title: "BEACON",
+                        address: "1035 Grant St SE Atlanta, GA 30315",
+                        user: "623de9958c010352ed1e3958",
+                        _id: "6248fb43e3c134faa1968df6",
+                    },
+                    {
+                        stationId: 141473,
+                        title: "COLUMBIA HONDA",
+                        address: "1650 Heriford Rd Columbia, MO 65202",
+                        user: "623de9958c010352ed1e3958",
+                        _id: "6248fb4ee3c134faa1968dfd",
+                    },
+                    {
+                        stationId: 72875,
+                        title: "WOOD PARTNERS",
+                        address: "4040 koehler Houston, TX 77007",
+                        user: "623de9958c010352ed1e3958",
+                        _id: "6248fb5ee3c134faa1968f32",
+                    },
+                ],
                 reviews: [],
             },
             {},
